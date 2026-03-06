3c4bd094a74d195e99f5f3fcd428792de0b2bb3b
sumankhavadia <sumansk101024@gmail.com>
2026-03-07 01:41:57 +0530

    correct cloudinary require path

--- backend/config/multerconfig.js
+++ backend/config/multerconfig.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../config/cloudinary.js"); // we'll create this next
+const cloudinary = require("./cloudinary.js");
 
 // Cloudinary storage — PDFs go directly to Cloudinary
 const storage = new CloudinaryStorage({
